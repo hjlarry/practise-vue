a5a72858507ea26a2bb436db7dfb0e5bbf8b9396
hjlarry <hjlarry@163.com>
2020-09-28 10:53:11 +0800

    add generators of js

--- es6/iterator.js
+++ es6/iterator.js
@@ -31,3 +31,44 @@ while (!result.done) {
   result = it.next();
 }
 console.log("Iterated over sequence of size: ", result.value); // 5
+
+/*
+二、 生成器
+*/
+
+// 使用生成器同样可以达到上例效果
+function* makeRangeIterator(start = 0, end = 100, step = 1) {
+  let iterationCount = 0;
+  for (let i = start; i < end; i += step) {
+    iterationCount++;
+    yield i;
+  }
+  return iterationCount;
+}
+
+// 生成器也可以使用yield接收数据
+function* fibonacci() {
+  let current = 0;
+  let next = 1;
+  while (true) {
+    let reset = yield current;
+    [current, next] = [next, next + current];
+    if (reset) {
+      current = 0;
+      next = 1;
+    }
+  }
+}
+
+const sequence = fibonacci();
+console.log(sequence.next().value);     // 0
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 2
+console.log(sequence.next().value);     // 3
+console.log(sequence.next().value);     // 5
+console.log(sequence.next().value);     // 8
+console.log(sequence.next(true).value); // 0
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 1
+console.log(sequence.next().value);     // 2
