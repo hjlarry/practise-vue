7d325a06aeea2a40d41e79fc9c473e394b9d3afb
hjlarry <hjlarry@163.com>
2020-10-14 16:06:03 +0800

    add a simple version of mvvm

---
+++ mvvm/simple/index.js
@@ -0,0 +1,20 @@
+class MVVM {
+  constructor(options) {
+    const { el, data, methods } = options;
+    this.methods = methods;
+    this.target = null;
+
+  }
+
+  observe(root, data) {
+
+  }
+
+  defineReactive(root, key, value) {
+
+  }
+
+  compile(dom) {
+
+  }
+}
